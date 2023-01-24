224ed8f18226c0140503bb819858624201221245
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2023-01-24 23:13:18 +0200

    Added the security measure to our api for attacks such as cross-site scripts, xss, hpp, data sanitization, and ect

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const { rateLimit } = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xssClean = require("xss-clean");
+const hpp = require("hpp");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -7,13 +12,31 @@ const errorController = require("./controllers/errorController");
 const AppError = require("./utils/AppError");
 
 const app = express();
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
 
 // 1) MIDDLEWARES
-// if (process.env.NODE_ENV === "development") {
+app.use(helmet());
 app.use(morgan("dev"));
-// }
-
-app.use(express.json());
+app.use("/api", limiter);
+app.use(express.json({ limit: "10kb" }));
+app.use(mongoSanitize());
+app.use(xssClean());
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsAverage",
+      "ratingsQuantity",
+      "difficulty",
+      "price",
+      "maxGroupSize",
+    ],
+  })
+);
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,15 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+    ),
+    secure: true,
+    httpOnly: true,
+  };
+
+  res.cookie("jwt", token, cookieOptions);
 
   res.status(201).json({
     status: "success",
